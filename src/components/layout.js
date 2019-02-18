a95491b77d361911177b7894a43af533528ffe4f
keevkeev <andmckvr@gmail.com>
2019-02-18 15:12:25 +0100

    added cdn links for slick-carousel

--- src/components/layout.js
+++ src/components/layout.js
@@ -40,6 +40,8 @@ const Layout = ({children}) => {
     >
       <html lang="en" />
       <link href="https://fonts.googleapis.com/css?family=Lato|Ubuntu" rel="stylesheet" />
+      <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
     </Helmet>
     <Navigation />
     <Content>
