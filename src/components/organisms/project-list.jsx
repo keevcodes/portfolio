a67d03390bb980e8e2c6e038cee6871e161c83ba
keevkeev <andmckvr@gmail.com>
2019-02-18 15:15:38 +0100

    added slick slider to project list, removed dummy stylings for project-list

--- src/components/organisms/project-list.jsx
+++ src/components/organisms/project-list.jsx
@@ -2,20 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 import ProjectCard from '../molecules/project-card';
+import Slider from 'react-slick';
 import { StaticQuery,  graphql } from 'gatsby';
-
-const ViewBox  = styled.div`
-  width: 100%;
-  overflow: scroll;
-`
-
-const List = styled.ul`
-  display: flex;
-  margin: 0;
-  list-style: none;
-  width: auto;
-  min-width: 200%;
-`
 
 const ProjectList = () => (
   <StaticQuery
@@ -45,13 +33,27 @@ const ProjectList = () => (
     }
   `}
   render={data => {
-    return (<ViewBox>
-    <List>
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      customPaging: i => (
+        <div style={{
+          backgroundColor: '#fff',
+          borderRadius: '50%',
+          height: '5px',
+          width: '5px'
+        }}></div>
+      )
+    };
+    return (
+    <Slider {...settings}>
       {data.allMarkdownRemark.edges.map(project => {
         return (<ProjectCard {...project} key={project.node.frontmatter.title} />)
       })}
-    </List>
-</ViewBox>)}} />
+    </Slider>)}} />
 )
 
 export default ProjectList;
