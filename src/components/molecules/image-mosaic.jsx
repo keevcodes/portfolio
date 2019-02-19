e7ed70684ddfe7aec3c0b2f5efda38b4526a025a
keevkeev <andmckvr@gmail.com>
2019-02-19 11:51:52 +0100

    reduced image maxWidths in query for smaller image sizes

--- src/components/molecules/image-mosaic.jsx
+++ src/components/molecules/image-mosaic.jsx
@@ -23,33 +23,35 @@ const ImageMosaic = () => (
       query {
         image1: file(relativePath: { eq: "bella_ingy_me.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 400) {
+            fluid(maxWidth: 200) {
               ...GatsbyImageSharpFluid
             }
           }
         }
         image2: file(relativePath: { eq: "ingy_me_wedding.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 600) {
+            fluid(maxWidth: 390) {
               ...GatsbyImageSharpFluid
             }
           }
         }
         image3: file(relativePath: { eq: "me_ingy_baloo.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 480) {
+            fluid(maxWidth: 280) {
               ...GatsbyImageSharpFluid
             }
           }
         }
       }
     `}
-    render={data => (
-      <ImageMosaicWrapper>
+    render={data => {
+      console.log(data);
+      return (
+        <ImageMosaicWrapper>
         <Img fluid={data.image2.childImageSharp.fluid} style={{gridColumn: '3/-1', gridRow: '1/8'}}/>
         <Img fluid={data.image1.childImageSharp.fluid} style={{gridColumn: '1/4', gridRow: '5/9', boxShadow: 'rgba(35, 37, 22, 0.6) 8px -2px 20px 2px'}}/>
         <Img fluid={data.image3.childImageSharp.fluid} style={{gridColumn: '4/-1', gridRow: '7/10', boxShadow: 'rgba(35,37,22, 0.6) -5px -5px 15px 2px'}}/>
       </ImageMosaicWrapper>
-    )} />)
+    )}} />)
 
 export default ImageMosaic;
