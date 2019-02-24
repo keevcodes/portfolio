cf38d4abc2dabd16215628a1c680f0420c7751f6
keevkeev <andmckvr@gmail.com>
2019-02-24 09:29:59 +0100

    ♻️ moved page data from metadata to json file
    Page data was previously in site meta data inside the gatsby-config file. Metadata should be reserved only for SEO metadata purposes. All page data is now inside its own json file

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,54 +13,16 @@ module.exports = {
       {icon: 'xing', link: 'https://www.xing.com/profile/Andrew_McKeever/cv'},
       {icon: 'github', link: 'https://github.com/keevkeev'},
       {icon: 'gmail', link: 'mailto:andmckvr@gmail.com'}
-    ],
-    home: {
-      desc: "Creative web developer based in Hamburg, Germany. I have experience working with brands such as Google, BMW and Adobe. Have a look around to learn more about me and my talents.",
-      headline: "Keeve.me"
-    },
-    about: {
-      headline: "About & Skills",
-      desc: "My web engineering journey began at the start of 2016 and have been honing my skill set ever since, working for big name clients like Adobe, BMW and Google. I'm passionate about front-end and growing with my team of developers and designers to create beautiful websites. Web development is an ever evolving and fast moving industry which fuels my desire to learn new technologies and experiement with interesting tools. My knowledge and tool set not only includes a solid foundation of HTML, CSS and Javascript, but also some of the latest tools like React, Redux, Webpack and GraphQl.  When not building web pages, I spend my time with my wife Ingrid, my daughter Isabella and my dog Baloo. I enjoy staying active in with boxing, snowboarding and when the weather permits, a good braai in the park."
-    },
-    work: {
-      desc: "",
-      headline: "Work & Experience",
-
-      jobs: [
-      {
-        company: 'Jung von Matt / TECH',
-        role: 'Junior Developer',
-        date: 'September 2017 - Present',
-        responsiblites: 'Frontend application and site development for various clients including Google, BMW and Bilder.de. Introduce ideas and new technologies to team members through presentations. Work with team members in an Agile enviornment and Srum teams.',
-        tools: ['React', 'ES6', 'CSS3 (Sass)', 'HTML (HTML5, Twig, Handlebars)', 'Responsive Design','Atomic Design', 'Agile development', 'AMP HTML' ]
-      },
-      {
-        company: 'Track',
-        role: 'Junior Developer',
-        date: 'January 2017 - August 2017',
-        responsiblites: 'Create email templates from scratch and prepare for email campaign shipment through Adobe Campaign tooling.',
-        tools: ['HTML', 'CSS', 'Adobe Campaign', 'Email Templating', 'Photoshop']
-      }
     ]
   },
-  contact: {
-    desc: "",
-    headline: "Contact.me"
-  }
-  },
   plugins: [
+    `gatsby-transformer-json`,
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
-
-    // relative string paths directly from siteMetaData are not yet supported by gatsbyImage,
-    // therefore projects were created as md files to include their images
-    // and assocaited data since gatsby knowns the location of the assoicated file paths with gatsby-source-filesystem.
-    // more info on this issue can be found here https://github.com/gatsbyjs/gatsby/issues/2968
       options: {
-        name: "projects",
-        path: `${__dirname}/src/projects/`,
-      },
+        path: `${__dirname}/src/data/`
+      }
     },
     {
       resolve: 'gatsby-plugin-react-svg',
@@ -90,6 +52,13 @@ module.exports = {
       options: {
         name: "skills",
         path: `${__dirname}/src/assets/skills/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "projects",
+        path: `${__dirname}/src/assets/project-images/`,
       },
     },
     'gatsby-plugin-react-helmet',
