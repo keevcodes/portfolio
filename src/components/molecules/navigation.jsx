0477fd9ebd175df77547f01c6dbda059368083c0
keevkeev <andmckvr@gmail.com>
2019-03-11 13:09:11 +0100

    💄 added hover animation to navigation items
    Fade in / out animation added to navaigation items on hover

--- src/components/molecules/navigation.jsx
+++ src/components/molecules/navigation.jsx
@@ -20,12 +20,42 @@ const List = styled.ul`
 `;
 
 const ListItem = styled.li`
-  color: #fff;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  bottom: 0%;
   padding-left: 15px;
 
+  .link {
+    text-decoration: none;
+    font-family: 'Lato, sans-serif';
+    opacity: 1;
+    margin-top: 0;
+    color: ${props => props.theme.white};
+    transition: all 0.2s ease;
+
+    &-hidden {
+      opacity: 0;
+      position: absolute;
+      top: 40%;
+      text-decoration: none;
+      color: ${props => props.theme.yellowGreen};
+    }
+  }
+
+
   &:hover {
-    content: '';
-    color: #DBFF76;
+    .link {
+      opacity: 0;
+      margin-top: -40%;
+      transition: all 0.3s ease;
+
+      &-hidden {
+        opacity: 1;
+        top: 0;
+        transition: all 0.3s ease;
+      }
+    }
   }
 `
 
@@ -49,9 +79,11 @@ const Navigation = () => (
       <Nav>
         <List>
           {data.site.siteMetadata.navigation.map((item) => {
-            return (<Link to={item.route} key={item.title} style={{textDecoration: 'none'}}>
-              <ListItem width="1.25em" height="1.75em" margin="0.5em">{item.title}</ListItem>
-            </Link>
+            return (
+              <ListItem width="1.25em" height="1.75em" margin="0.5em" key={item.title} title={item.title} >
+                <Link to={item.route} className="link">{item.title}</Link>
+                <Link to={item.route} className="link-hidden">{item.title}</Link>
+              </ListItem>
           )
           })}
         </List>
