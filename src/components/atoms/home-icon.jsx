8fad1d2f4304ccb700f7671308af94b6db5f8748
keevkeev <andmckvr@gmail.com>
2019-05-05 12:27:08 +0200

    🍱 brought back kiwi bird icon into icons.svg
    bird icon was removed from the background svg, but is no brought back in

--- src/components/atoms/home-icon.jsx
+++ src/components/atoms/home-icon.jsx
@@ -3,25 +3,54 @@ import styled from 'styled-components';
 
 import '../../assets/icons.svg';
 
-const Icon = styled.div`
+const Container = styled.div`
+  height: 50vh;
+
+  @media (min-width: 768px) {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`
+const BackgroundIcon = styled.div`
   display: flex;
-  min-width: 60%;
   align-items: center;
+  height: 100%;
 
   @media screen and (min-width: 768px) {
     justify-content: center;
+    height: 100vh;
+    width: 55vw;
   }
 
   & svg {
-    height: 100vh;
+    height: 100%;
     width: 100%;
+  }
+
+`
+const BirdIcon = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  top: 25%;
+  width: calc(90vw - 5px);
+
+  @media (min-width: 768px) {
+    height: calc(100% - 150px);
+    top: auto;
   }
 `
 
-const HomeIcon = () => (
-  <Icon>
+const HomeIcon = () => (<Container>
+  <BackgroundIcon>
     <svg><use xlinkHref="#icons_kiwi-background"/></svg>
-  </Icon>
-)
+  </BackgroundIcon>
+  <BirdIcon>
+    <svg><use xlinkHref="#icons_kiwi-bird"/></svg>
+  </BirdIcon>
+</Container>)
 
 export default HomeIcon;
