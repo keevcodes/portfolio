3ccffb32668827c84822f61263bd83c62f3317b4
keevkeev <andmckvr@gmail.com>
2019-05-10 20:47:23 +0200

    💄 style changes and css cleanup for navigation

--- src/components/molecules/navigation.jsx
+++ src/components/molecules/navigation.jsx
@@ -1,49 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link, StaticQuery,  graphql } from 'gatsby';
+import { MediumText } from '../atoms/text';
 
-const Nav = styled.nav`
-  width: 90vw;
-  margin: 0 auto;
-  padding: 20px 0 0;
-  z-index: 10;
-  background: transparent;
+const List = styled.ul`
   display: flex;
   justify-content: flex-end;
-  align-items: center;
-`
-const List = styled.ul`
   list-style: none;
-  position: relative;
-  display: flex;
-  margin: 0;
+  width: 90vw;
+  margin: 0 auto;
+  padding-top: 20px;
+
 `;
 
 const ListItem = styled.li`
   display: flex;
-  flex-direction: column;
   position: relative;
   bottom: 0%;
   padding-left: 15px;
 
   .link {
     text-decoration: none;
-    font-family: 'Lato, sans-serif';
     opacity: 1;
     margin-top: 0;
-    color: ${props => props.theme.white};
     transition: all 0.2s ease;
 
     &-hidden {
-      opacity: 0;
-      position: absolute;
-      top: 40%;
-      text-decoration: none;
-      color: ${props => props.theme.yellowGreen};
+      display: none;
     }
   }
 
   @media (min-width: 980px) {
+    flex-direction: column;
+
+
+    & .link-hidden {
+        display: flex;
+        opacity: 0;
+        position: absolute;
+        top: 40%;
+        text-decoration: none;
+        color: ${props => props.theme.yellowGreen};
+      }
+
     &:hover {
       .link {
         opacity: 0;
@@ -77,18 +76,18 @@ const Navigation = () => (
     }
   `}
     render={data => (
-      <Nav>
+      <nav>
         <List>
           {data.site.siteMetadata.navigation.map((item) => {
             return (
-              <ListItem width="1.25em" height="1.75em" margin="0.5em" key={item.title} title={item.title} >
-                <Link to={item.route} className="link">{item.title}</Link>
-                <Link to={item.route} className="link-hidden">{item.title}</Link>
+              <ListItem key={item.title} title={item.title} >
+                <Link to={item.route} className="link"><MediumText text={item.title} /></Link>
+                <Link to={item.route} className="link-hidden"><MediumText highlight text={item.title} /></Link>
               </ListItem>
           )
           })}
         </List>
-      </Nav>
+      </nav>
     )}/>
 )
 
